252efda222a55cf51eaead00f56dbeeb08b44858
Me <bryan.loertscher@missionary.org>
2025-01-25 16:43:36 -0600

    rename StoryBase.set_accepting_new_players() to set_game_started()

--- code/flask_app/app.py
+++ code/flask_app/app.py
@@ -88,7 +88,7 @@ def get_updates () :
 def start_game () :
     if request.method == "POST":
         # do something about room starting
-        storybase.set_accepting_new_players(True)
+        storybase.set_game_started(True)
         return redirect(url_for("writing_page"))
 
 @app.route("/check-game-start", methods=["GET"])
